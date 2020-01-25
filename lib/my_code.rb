0da51a3ac763af18f12820240e3f02887f142463
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 03:44:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(array)
   #map returns new array of manipulated elements
   new = [] #new array to be returned
   i = 0
-  while i < array.length
+  while i < array.size
     new.push(yeild(array[i])) #push each result from yeild into the new array
     i += 1
   end
